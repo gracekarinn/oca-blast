d0ec797a031381827523ce7e52bf76fdd60f0781
gracekarinn <gracekarina12@gmail.com>
2024-09-15 21:39:36 +0700

    feat: add email module for managing email functionality

---
+++ src/email/email.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Email, EmailSchema } from 'src/schemas/Email.schema';
+import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Email.name,
+        schema: EmailSchema,
+      },
+    ]),
+  ],
+  providers: [EmailService],
+  controllers: [EmailController],
+})
+export class EmailModule {}
