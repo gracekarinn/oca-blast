22ab04c0f90c9cb0a16f77e2cedeeeb4b5b0b8c3
gracekarinn <gracekarina12@gmail.com>
2024-09-15 23:40:00 +0700

    feat: add crud operations for email management

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,13 +1,19 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpException,
+  Param,
+  Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { CreateEmailDto } from './dto/CreateEmail.dto';
+import mongoose from 'mongoose';
+import { UpdateEmailDto } from './dto/UpdateEmail.dto';
 
 @Controller('email')
 export class EmailController {
@@ -22,5 +28,48 @@ export class EmailController {
   @Get()
   getEmail() {
     return this.emailService.getEmail();
+  }
+
+  @Get(':id')
+  async getEmailById(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) {
+      throw new HttpException('Email not found', 404);
+    }
+    const email = await this.emailService.getEmailById(id);
+    if (!email) {
+      throw new HttpException('Email not found', 404);
+    }
+    return email;
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe())
+  async updateEmail(
+    @Param('id') id: string,
+    @Body() updateEmailDto: UpdateEmailDto,
+  ) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) {
+      throw new HttpException('Email not found', 404);
+    }
+    const email = await this.emailService.updateEmail(id, updateEmailDto);
+    if (!email) {
+      throw new HttpException('Email not found', 404);
+    }
+    return email;
+  }
+
+  @Delete(':id')
+  async deleteEmail(@Param('id') id: string) {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) {
+      throw new HttpException('Email not found', 404);
+    }
+    const email = await this.emailService.deleteEmail(id);
+    if (!email) {
+      throw new HttpException('Email not found', 404);
+    }
+    return email;
   }
 }
