557183c46fd75862a2ce65337d91e0782f127c2f
gracekarinn <gracekarina12@gmail.com>
2024-09-16 17:17:23 +0700

    feat: connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/oca'), EmailModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
